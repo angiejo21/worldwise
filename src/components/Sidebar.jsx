bf9b6eeed5180032fc334f9bbf296ebdef731a7f
Angela Bellò <angela.bello90@gmail.com>
2023-12-06 16:53:36 +0100

    + cities and countries lists

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,13 +2,14 @@ import AppNav from "./AppNav";
 import Logo from "./Logo";
 import Footer from "./Footer";
 import styles from "./Sidebar.module.css";
+import { Outlet } from "react-router-dom";
 
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>list of cities</p>
+      <Outlet />
       <Footer />
     </div>
   );
